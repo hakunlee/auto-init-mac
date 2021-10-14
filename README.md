88ae0223287c40f214678ccc6067242187918b14
hakunlee <33917986+hakunlee@users.noreply.github.com>
2021-10-14 18:04:18 +0900

    Initialize Mac OS automatically

---
+++ README.md
@@ -0,0 +1,5 @@
+### How to use
+`> ./install.sh`
+
+### Included software
+[See Brewfile](https://github.com/hakunlee/auto-init-mac/blob/main/Brewfile)
---
+++ install.sh
@@ -0,0 +1,21 @@
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# install via brew
+brew bundle --file=./Brewfile
+
+sudo xattr -dr com.apple.quarantine /Applications/GitKraken.app
+sudo xattr -dr com.apple.quarantine /Applications/MySQLWorkbench.app
+sudo xattr -dr com.apple.quarantine /Applications/Postman.app
+sudo xattr -dr com.apple.quarantine /Applications/Google\ Chrome.app
+sudo xattr -dr com.apple.quarantine /Applications/iTerm.app
+sudo xattr -dr com.apple.quarantine /Applications/KakaoTalk.app
+sudo xattr -dr com.apple.quarantine /Applications/Owly.app
+sudo xattr -dr com.apple.quarantine /Applications/IntelliJ\ IDEA.app
+sudo xattr -dr com.apple.quarantine /Applications/Slack.app
+sudo xattr -dr com.apple.quarantine /Applications/Docker.app
+
+# configure zsh
+chmod 755 ./zsh/install.sh
+./zsh/install.sh
+
